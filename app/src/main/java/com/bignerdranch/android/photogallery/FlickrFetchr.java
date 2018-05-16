c2e651ca91912e37889e366a4466c1ae40685508
Ben-DG <bendgrafstein@gmail.com>
2018-05-15 23:14:06 -0700

    Challenge: Gson

--- app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.java
@@ -3,6 +3,9 @@ package com.bignerdranch.android.photogallery;
 import android.net.Uri;
 import android.util.Log;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -10,6 +13,7 @@ import org.json.JSONObject;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
@@ -78,22 +82,14 @@ public class FlickrFetchr {
     private void parseItems(List<GalleryItem> items, JSONObject jsonBody)
         throws IOException, JSONException {
 
+        Gson gson = new Gson();
+        Type galleryItemType = new TypeToken<ArrayList<GalleryItem>>(){}.getType();
+
         JSONObject photosJsonObject = jsonBody.getJSONObject("photos");
         JSONArray photosJsonArray = photosJsonObject.getJSONArray("photo");
+        String jsonPhotosString = photosJsonArray.toString();
 
-        for (int i = 0; i < photosJsonArray.length(); i++) {
-            JSONObject photoJsonObject = photosJsonArray.getJSONObject(i);
-
-            GalleryItem item = new GalleryItem();
-            item.setId(photoJsonObject.getString("id"));
-            item.setCaption(photoJsonObject.getString("title"));
-
-            if (!photoJsonObject.has("url_s")) {
-                continue;
-            }
-
-            item.setUrl(photoJsonObject.getString("url_s"));
-            items.add(item);
-        }
+        List<GalleryItem> galleryItemList =  gson.fromJson(jsonPhotosString, galleryItemType);
+        items.addAll(galleryItemList);
     }
 }
