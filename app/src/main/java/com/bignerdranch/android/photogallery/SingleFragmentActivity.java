58909f485e992f4315f199bd35280b744f549f6d
Ben-DG <bendgrafstein@gmail.com>
2018-05-15 22:24:56 -0700

    Up to end ch 25

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/SingleFragmentActivity.java
@@ -0,0 +1,38 @@
+package com.bignerdranch.android.photogallery;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ben on 7/11/2017.
+ */
+
+// criminal intent uses AppCompat library, so all activities are a subclass of AppCompatActivity
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId()); // Enables a subclass to provide its own layout id instead
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
